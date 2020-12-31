7eab149c5d43915390aae737de7019b9ab46f924
Chu Phong <chucamphong1999@gmail.com>
2020-12-31 21:11:51 +0700

    Refactor: Tối ưu code và thêm thanh Loading

--- app/src/main/java/com/music/fragments/AlbumSliderFragment.java
+++ app/src/main/java/com/music/fragments/AlbumSliderFragment.java
@@ -11,63 +11,56 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.music.R;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.music.adapters.AlbumSliderAdapter;
+import com.music.databinding.FragmentImageSliderBinding;
 import com.music.models.Album;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AlbumSliderFragment extends Fragment {
-    @SuppressWarnings("FieldCanBeLocal")
-    private FirebaseFirestore mFirestore;
-
-    @SuppressWarnings("FieldCanBeLocal")
-    private CollectionReference mCollection;
-
-    private ViewPager2 mViewPager;
-
-    private final List<Album> mAlbums = new ArrayList<>();
+    private FragmentImageSliderBinding mBinding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mFirestore = FirebaseFirestore.getInstance();
-        mCollection = mFirestore.collection("albums");
+        FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+        CollectionReference mCollection = mFirestore.collection("albums");
 
         mCollection.whereEqualTo("recommend", true).get()
-                .addOnSuccessListener(query -> {
-                    for (DocumentSnapshot document : query.getDocuments()) {
-                        mAlbums.add(document.toObject(Album.class));
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        QuerySnapshot query = task.getResult();
+
+                        List<Album> albums = query.getDocuments().stream()
+                                .map(document -> document.toObject(Album.class))
+                                .collect(Collectors.toList());
+
+                        mBinding.imageSlider.setAdapter(new AlbumSliderAdapter(albums, mBinding.imageSlider));
+                    } else {
+                        Toast.makeText(getActivity(), "Không thể tải album bài hát.", Toast.LENGTH_SHORT).show();
                     }
 
-                    if (mViewPager.getAdapter() != null) {
-                        mViewPager.getAdapter().notifyDataSetChanged();
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    Toast.makeText(getActivity(), "Không thể tải album bài hát.", Toast.LENGTH_SHORT).show();
+                    mBinding.prbLoading.setVisibility(View.GONE);
                 });
     }
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_image_slider, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mBinding = FragmentImageSliderBinding.inflate(inflater, container, false);
+
+        return mBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        mViewPager = view.findViewById(R.id.image_slider);
-
-        mViewPager.setAdapter(new AlbumSliderAdapter(mAlbums, mViewPager));
 
         // Thêm hiệu ứng chuyển cảnh khi cuộn hình ảnh
         CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
@@ -77,6 +70,6 @@ public class AlbumSliderFragment extends Fragment {
             page.setScaleY(0.85f + r * 0.15f);
         });
 
-        mViewPager.setPageTransformer(compositePageTransformer);
+        mBinding.imageSlider.setPageTransformer(compositePageTransformer);
     }
 }
