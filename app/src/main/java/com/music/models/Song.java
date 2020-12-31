aa38241676d341583685a115fe08762963816f5f
Chu Phong <chucamphong1999@gmail.com>
2020-12-31 19:31:15 +0700

    Add: Song model

---
+++ app/src/main/java/com/music/models/Song.java
@@ -0,0 +1,84 @@
+package com.music.models;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.DocumentId;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.PropertyName;
+
+import org.apache.commons.text.WordUtils;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Song {
+    /**
+     * ID bài hát
+     */
+    @DocumentId
+    private String id;
+
+    /**
+     * Tên bài hát
+     */
+    @NonNull
+    private String name;
+
+    /**
+     * Tên khác của bài hát
+     */
+    @Nullable
+    private String otherName;
+
+    /**
+     * Đường dẫn hình ảnh
+     */
+    @NonNull
+    private String image;
+
+    /**
+     * Lượt nghe của bài hát
+     */
+    private long views;
+
+    /**
+     * Danh sách nghệ sĩ
+     */
+    @NonNull
+    private List<DocumentReference> artists;
+
+    /**
+     * Viết hoa ký tự đầu của mỗi từ trong tên bài hát
+     */
+    public void setName(@NonNull String name) {
+        this.name = WordUtils.capitalize(name);
+    }
+
+    /**
+     * Lấy tên khác của bài hát
+     *
+     * @return Tên khác của bài hát
+     */
+    @Nullable
+    @SuppressWarnings("unused")
+    @PropertyName("other_name")
+    public String getOtherName() {
+        return otherName;
+    }
+
+    /**
+     * Gán tên khác cho bài hát
+     *
+     * @param otherName Tên khác của bài hát
+     */
+    @SuppressWarnings("unused")
+    @PropertyName("other_name")
+    public void setOtherName(@Nullable String otherName) {
+        this.otherName = otherName;
+    }
+}
