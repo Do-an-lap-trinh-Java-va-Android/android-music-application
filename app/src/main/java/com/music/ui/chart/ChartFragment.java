3a23e8740d956c47f8221fd372b043df60442f5a
Chu Phong <chucamphong1999@gmail.com>
2021-01-04 16:49:14 +0700

    Add: Thêm Chart Fragment

---
+++ app/src/main/java/com/music/ui/chart/ChartFragment.java
@@ -0,0 +1,36 @@
+package com.music.ui.chart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.music.databinding.FragmentChartBinding;
+
+public class ChartFragment extends Fragment {
+    @SuppressWarnings("NotNullFieldNotInitialized")
+    @NonNull
+    private FragmentChartBinding binding;
+
+    @SuppressWarnings("NotNullFieldNotInitialized")
+    @NonNull
+    private ChartViewModel chartViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentChartBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        chartViewModel = new ViewModelProvider(this).get(ChartViewModel.class);
+    }
+}
---
+++ app/src/main/java/com/music/ui/chart/ChartViewModel.java
@@ -0,0 +1,13 @@
+package com.music.ui.chart;
+
+import androidx.hilt.Assisted;
+import androidx.hilt.lifecycle.ViewModelInject;
+import androidx.lifecycle.SavedStateHandle;
+import androidx.lifecycle.ViewModel;
+
+public class ChartViewModel extends ViewModel {
+    @ViewModelInject
+    public ChartViewModel(@Assisted SavedStateHandle savedStateHandle) {
+
+    }
+}
