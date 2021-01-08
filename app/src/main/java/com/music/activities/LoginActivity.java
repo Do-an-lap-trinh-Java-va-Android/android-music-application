c2ee725d34f03c56b8d85180e47a274610c33328
Quoc Khai <khai.cq.27@gmail.com>
2021-01-08 19:14:39 +0700

    Login Activity

--- app/src/main/java/com/music/activities/LoginActivity.java
+++ app/src/main/java/com/music/activities/LoginActivity.java
@@ -1,16 +1,60 @@
 package com.music.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.music.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.music.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        binding.btnLogin.setOnClickListener(v -> {
+            String email = binding.edtFullName.getText().toString().trim();
+            String password = binding.edtPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                binding.edtFullName.setError("không được bỏ trống");
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                binding.edtPassword.setError("Bỏ trống là không được");
+                return;
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this,
+                        "Địa chỉ Email không hợp lệ", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this,
+                                    "Đăng nhập thành công",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            binding.edtFullName.setError("Tài khoản hoặc mật khẩu không đúng");
+                        }
+                    });
+        });
     }
 }
