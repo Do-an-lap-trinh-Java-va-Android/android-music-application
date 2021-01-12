a094abed8a868def4bdc545e7e8ca38c0ab46c5e
Chu Phong <chucamphong1999@gmail.com>
2021-01-12 22:41:34 +0700

    Feat: Hiển thị tên và ảnh đại diện của tài khoản

--- app/src/main/java/com/music/ui/account/AccountFragment.java
+++ app/src/main/java/com/music/ui/account/AccountFragment.java
@@ -15,11 +15,17 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseUser;
 import com.music.R;
 import com.music.databinding.FragmentAccountBinding;
 
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class AccountFragment extends Fragment {
-    @Nullable
+    @SuppressWarnings("NotNullFieldNotInitialized")
+    @NonNull
     private FragmentAccountBinding binding;
 
     @SuppressWarnings({"NotNullFieldNotInitialized", "FieldCanBeLocal"})
@@ -27,7 +33,8 @@ public class AccountFragment extends Fragment {
     private AccountViewModel viewModel;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
 
@@ -39,6 +46,16 @@ public class AccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         viewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+
+        FirebaseUser user = viewModel.getCurrentUser();
+
+        Glide.with(binding.ivUserAvatar.getContext())
+                .load(user.getPhotoUrl())
+                .circleCrop()
+                .fallback(R.drawable.purple_gradient_background)
+                .into(binding.ivUserAvatar);
+
+        binding.tvHelloUser.setText(user.getDisplayName());
     }
 
     @Override
@@ -49,13 +66,14 @@ public class AccountFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.menu_setting) {
-            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
             navController.navigate(R.id.navigation_setting_fragment);
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings({"AssignmentToNull", "ConstantConditions"})
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/music/ui/account/AccountViewModel.java
+++ app/src/main/java/com/music/ui/account/AccountViewModel.java
@@ -1,7 +1,25 @@
 package com.music.ui.account;
 
+import androidx.annotation.NonNull;
+import androidx.hilt.lifecycle.ViewModelInject;
 import androidx.lifecycle.ViewModel;
 
-public class AccountViewModel extends ViewModel {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
+public class AccountViewModel extends ViewModel {
+    @NonNull
+    private final FirebaseUser currentUser;
+
+    @ViewModelInject
+    public AccountViewModel(@NonNull FirebaseAuth firebaseAuth) {
+        currentUser = Objects.requireNonNull(firebaseAuth.getCurrentUser());
+    }
+
+    @NonNull
+    public FirebaseUser getCurrentUser() {
+        return currentUser;
+    }
 }
