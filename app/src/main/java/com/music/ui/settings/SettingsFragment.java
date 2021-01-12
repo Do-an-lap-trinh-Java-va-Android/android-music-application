2b06926dbb8a200a7f8ee377c4318e908eab1ef1
Chu Phong <chucamphong1999@gmail.com>
2021-01-12 22:05:33 +0700

    Fix: Sửa lỗi nút đăng xuất không hoạt động khi click lần đầu

--- app/src/main/java/com/music/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/music/ui/settings/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.music.ui.settings;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -10,9 +11,17 @@ import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.music.R;
 
+import javax.inject.Inject;
+
 public class SettingsFragment extends PreferenceFragmentCompat implements OnPreferenceChangeListener, OnPreferenceClickListener {
+    private static final String TAG = "SettingsFragment";
+
+    @Inject
+    FirebaseAuth firebaseAuth;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.setting_preferences, rootKey);
@@ -33,7 +42,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if ("change_app_theme".equals(preference.getKey())) {
-            switchThemePreferenceChangeListener(preference, newValue);
+            handleSwithTheme(preference, newValue);
         }
 
         return true;
@@ -42,29 +51,46 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     @Override
     public boolean onPreferenceClick(Preference preference) {
         if ("logout".equals(preference.getKey())) {
-            preference.setOnPreferenceClickListener(_preference -> {
-                FirebaseAuth.getInstance().signOut();
-                return true;
-            });
+            handleLogout();
         }
 
         return true;
     }
 
-    private void switchThemePreferenceChangeListener(@NonNull Preference preference,
-                                                     @NonNull Object newValue) {
+    /**
+     * Xử lý thao tác chuyển đổi giao diện đen, trắng, mặc định
+     *
+     * @param preference Preference
+     * @param newValue   Giao diện muốn chuyển đến
+     */
+    private void handleSwithTheme(@NonNull Preference preference, @NonNull Object newValue) {
         String theme = newValue.toString();
 
         switch (theme) {
             case "dark":
+                Log.i(TAG, "handleSwithTheme: Đã chuyển sang giao diện tối");
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 break;
             case "light":
+                Log.i(TAG, "handleSwithTheme: Đã chuyển sang giao diện sáng");
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 break;
             default:
+                Log.i(TAG, "handleSwithTheme: Đã chuyển sang giao diện theo hệ thống");
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 break;
+        }
+    }
+
+    /**
+     * Xử lý thao tác đăng xuất
+     */
+    private void handleLogout() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null) {
+            Log.i(TAG, "onPreferenceClick: Đăng xuất tài khoản: " + user.getEmail());
+            firebaseAuth.signOut();
         }
     }
 }
